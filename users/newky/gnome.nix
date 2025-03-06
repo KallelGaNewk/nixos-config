3efd08b478f5402437fc19be057bc849d3314975
KallelGaNewk <kallelgn@gmail.com>
2025-03-06 16:27:32 -0300

    disable GNOME screen dimming

--- users/newky/gnome.nix
+++ users/newky/gnome.nix
@@ -12,5 +12,10 @@
 
     # Dark mode interface
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+    # Disable screen dimming
+    "org/gnome/desktop/session".idle-delay = 0; # default: 300
+    "org/gnome/settings-daemon/plugins/power".sleep-inactive-ac-type = "nothing"; # default: suspend
+    "org/gnome/settings-daemon/plugins/power".idle-brightness = 100; # default: 30
   };
 }
